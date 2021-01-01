c147a42b5d8f05f7d3e6ff42a247e073055a9606
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-01 15:40:06 +0200

    Implement SRP
    Implements SRP by introducing 3 new classes for different responsibilities + tests for JsonPolicySerializer

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace ArdalisRating
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ArdalisRating
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString,
+                new StringEnumConverter());
+        }
+    }
+}
